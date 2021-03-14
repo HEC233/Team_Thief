e8d2a859b91c4b5f48f59bdbd9bcf22f98a6bf09
Kojaehyeop <wja04003@naver.com>
2021-03-14 17:23:42 +0900

    베이스 코드 함수구조를 간단하게 작성함

--- Project_Team_Thief/Assets/Game/Script/Manager/CameraManager.cs
+++ Project_Team_Thief/Assets/Game/Script/Manager/CameraManager.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Shake()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Zoom()
     {
-        
+
     }
 }
