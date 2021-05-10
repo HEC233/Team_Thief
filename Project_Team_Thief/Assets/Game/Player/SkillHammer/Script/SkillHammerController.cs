52bb1238968ff08552f8a8cae89fc936e02f17ad
KImTaeSung <xotjdvkdnj@naver.com>
2021-05-10 14:53:45 +0900

    스킬3 해머 구현 준비

---
+++ Project_Team_Thief/Assets/Game/Player/SkillHammer/Script/SkillHammerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillHammerController : SkillControllerBase
+{
+    public SkillHammerController(GameSkillObject skillObject, SkillDataBase data, Unit unit) : base(skillObject, data, unit) { }
+
+    private SkillHammerData _skillHammerData;
+
+    private PlayerUnit _unit;
+
+    private Damage _damage;
+    
+    public override void Invoke()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        
+    }
+}
---
+++ Project_Team_Thief/Assets/Game/Player/SkillHammer/Script/SkillHammerData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SkillHammerData", menuName = "ScriptableObject/SkillHammerData")]
+public class SkillHammerData : SkillDataBase
+{
+    private SignalSourceAsset _cinemachineSignalSource;
+    
+    public SignalSourceAsset CinemachineSignalSource => _cinemachineSignalSource;
+    
+    [SerializeField] 
+    private Vector2 _knockBackPower;
+    public Vector2 KnockBackPower => _knockBackPower;
+    
+    [SerializeField]
+    private float _attackDamage;
+    public float AttackDamage => _attackDamage;
+    
+    public override SkillControllerBase GetSkillController(GameSkillObject skillObject, Unit unit)
+    {
+        return new SkillHammerController(skillObject, this, unit);
+    }
+}
