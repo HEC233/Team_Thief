9e44d24d9ffc7b3cc1d93592eeb1169c860042c2
Kojaehyeop <wja04003@naver.com>
2021-05-16 00:58:04 +0900

    세라핌 사격에 있는 라이트 삭제

--- Project_Team_Thief/Assets/Game/Script/Unit/Enemy/Seraphim/SeraphimFireFXCtrl.cs
+++ Project_Team_Thief/Assets/Game/Script/Unit/Enemy/Seraphim/SeraphimFireFXCtrl.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class SeraphimFireFXCtrl : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
-    public GameObject light;
+    //public GameObject light;
     private float[] thisPos = new float[2];
-    private float[] lightPos = new float[2];
+    //private float[] lightPos = new float[2];
 
     private void Awake()
     {
         thisPos[0] = transform.localPosition.x;
         thisPos[1] = -thisPos[0];
-        lightPos[0] = light.transform.localPosition.x;
-        lightPos[1] = -lightPos[0];
+        //lightPos[0] = light.transform.localPosition.x;
+        //lightPos[1] = -lightPos[0];
 
     }
 
@@ -27,6 +27,6 @@ public class SeraphimFireFXCtrl : MonoBehaviour
     {
         spriteRenderer.flipX = value;
         transform.localPosition = new Vector3(thisPos[value ? 1 : 0], transform.localPosition.y, transform.localPosition.z);
-        light.transform.localPosition = new Vector3(lightPos[value ? 1 : 0], light.transform.localPosition.y, light.transform.localPosition.z);
+        //light.transform.localPosition = new Vector3(lightPos[value ? 1 : 0], light.transform.localPosition.y, light.transform.localPosition.z);
     }
 }
