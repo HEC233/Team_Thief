54ac9573937192b21a8b9b52cc277dee62179093
KImTaeSung <xotjdvkdnj@naver.com>
2021-05-18 20:44:43 +0900

    WwiseSoundManager 구현 및 간단하게 적용 중

---
+++ Project_Team_Thief/Assets/Game/Sound/Script/TestSoundScrpit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestSoundScrpit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void PlaySound()
+    {
+        AkSoundEngine.PostEvent("PC_HIT_hammer", gameObject);
+    }
+
+    public void PlaySound2()
+    {
+        AkSoundEngine.PostEvent("PC_BA2", gameObject);
+    }
+}
