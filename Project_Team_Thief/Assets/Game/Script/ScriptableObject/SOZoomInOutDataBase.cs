4c79a4cd1855d600352ef8c5684e6404a2e999b8
KImTaeSung <xotjdvkdnj@naver.com>
2021-09-07 15:50:50 +0900

    타일 팔레트 제작 및 카메라 줌인아웃 데이터 생성 중이였음

---
+++ Project_Team_Thief/Assets/Game/Script/ScriptableObject/SOZoomInOutDataBase.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SOZoomInOutData", menuName = "ScriptableObject//SOZoomInOutData")]
+public class SOZoomInOutDataBase : ScriptableObject
+{
+    [SerializeField] 
+    protected AnimationCurve _zoomInCurve;
+    [SerializeField] 
+    protected float _zoomInTime;
+    [SerializeField] 
+    protected float _zoomInSize;
+    [SerializeField] 
+    protected AnimationCurve _zoomOutCurve;
+    [SerializeField]
+    protected float _zoomOutTime;
+    
+    protected CinemachineBlendDefinition _cinemachineBlendDefinition;
+
+    public void ZoomIn()
+    {
+        _cinemachineBlendDefinition.m_Style = CinemachineBlendDefinition.Style.Custom;
+        _cinemachineBlendDefinition.m_CustomCurve = _zoomInCurve;
+        _cinemachineBlendDefinition.m_Time = _zoomInTime;
+
+        GameManager.instance.CameraMng.ZoomIn(_cinemachineBlendDefinition, _zoomInSize);
+    }
+    
+    private void ZoomOut()
+    {
+        _cinemachineBlendDefinition.m_Style = CinemachineBlendDefinition.Style.Custom;
+        _cinemachineBlendDefinition.m_CustomCurve = _zoomOutCurve;
+        _cinemachineBlendDefinition.m_Time = _zoomOutTime;
+        
+        GameManager.instance.CameraMng.ZoomOut(_cinemachineBlendDefinition);
+    }
+    
+}
